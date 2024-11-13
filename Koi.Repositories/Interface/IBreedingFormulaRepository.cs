1cf3d00f430c1a8849cac938c64d8c913c6d6755
baootram <baotram2020tg@gmail.com>
2024-11-13 18:56:33 +0700

    Delete Koi.Repositories directory

--- Koi.Repositories/Interface/IBreedingFormulaRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-using Koi.Repositories.Base;
-using Koi.Repositories.Models;
-
-public interface IBreedingFormulaRepository : IGenericRepository<BreedingFormula>
-{
-    Task<BreedingFormula?> GetByFatherAndMotherBreedAsync(string fatherBreed, string motherBreed);
-    Task<IEnumerable<BreedingFormula>> GetAllFormulasAsync();
-}
