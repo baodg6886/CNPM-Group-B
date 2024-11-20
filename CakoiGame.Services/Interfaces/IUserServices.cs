d13ccb00313688b718dd1e9a98e24142d083c1a0
Thuc2606 <phamdinhthuc839@gmail.com>
2024-11-21 01:23:31 +0700

    Add files via upload

---
+++ CakoiGame.Services/Interfaces/IUserServices.cs
@@ -0,0 +1,14 @@
+﻿
+using CakoiGame.Repositories.Entities;
+
+namespace CakoiGame.Services.Interfaces
+{
+    public interface IUserSvc
+    {
+        Task<List<QlUser>> GetAllUsersAsync();
+        Task<QlUser> GetAllUserByIdAsync(int id);
+        Task EditUserAsync(QlUser qlUser);
+        Task CreateUserAsync(QlUser qlUser);
+        Task DeleteUserAsync(int id);
+    }
+}
